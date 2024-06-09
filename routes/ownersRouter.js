a7a1b4d608a7f4e517b988dfc5e798bb1a92822e
BeastxD7 <shashank.21ai047@sode-edu.in>
2024-06-10 04:59:32 +0530

    Updated Owners Route

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,29 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner.model");
 
-router.get('/',(req, res)=>{
-    res.send('hey its working');
+router.get("/", (req, res) => {
+  res.send("hey its working");
 });
 
-module.exports = router
+router.post("/create", async (req, res) => {
+
+
+  let owners = await ownerModel.find();
+  if (owners.length > 0){
+    return res.send(504).send("you don't have permission to create owner.");
+}
+
+    const {fullname,email,password} = req.body;
+
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+    });
+    res.status(200).send(createdOwner)
+
+});
+
+module.exports = router;
+ 
